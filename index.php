de212862b657ddbcbe48c7abf7638c19219bc0a9
Theislemanager <143001364+Theislemanager@users.noreply.github.com>
2023-08-23 17:55:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-    include __DIR__ . '/src/rcon.php';
-    $rcon = new RconClient('your_server_ip', 'your_rcon_port', 'your_rcon_password');
-    if($rcon->connect()){
-        $response = $rcon->sendCommand('announce', 'Hello World');
-        //$response = $rcon->sendCommand('kick', 'STEAMID64');
-        echo "$response\n";
-    }else{
-        echo "Could not connect";
-    }
+include __DIR__ . '/src/rcon.php';
+$rcon = new RconClient('your_server_ip', 'your_rcon_port', 'your_rcon_password');
+if($rcon->connect()){
+$response = $rcon->sendCommand('announce', 'Hello World');
+//$response = $rcon->sendCommand('kick', 'STEAMID64');
+echo $response.PHP_EOL;
+}else{
+echo 'Could not connect';
+}
