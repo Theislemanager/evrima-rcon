6d15d7d4b18fe092f37466d8d68bff2910f70f05
Theislemanager <143001364+Theislemanager@users.noreply.github.com>
2023-08-23 16:11:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-    require_once('src/rcon.php');
+<?php
+    include __DIR__ . '/src/rcon.php';
     $rcon = new RconClient("your_server_ip", "your_rcon_port", "your_rcon_password");
     if($rcon->connect()){
         $response = $rcon->sendCommand('announce', 'Hello World');
