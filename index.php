035c8cf58ed884e193a560f61df0d37c040e38ee
Theislemanager <143001364+Theislemanager@users.noreply.github.com>
2023-08-23 16:09:33 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+    require_once('src/rcon.php');
+    $rcon = new RconClient("your_server_ip", "your_rcon_port", "your_rcon_password");
+    if($rcon->connect()){
+        $response = $rcon->sendCommand('announce', 'Hello World');
+        echo "$response\n";
+    }else{
+        echo "Could not connect";
+    }
